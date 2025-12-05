a9d0cc69a869ab70a4fe4b8e3f2fd46a4b2fd356
samkline-lab <samkline@seattleacademy.org>
2025-12-05 17:22:17 +0000

    project 1

--- project1.py
+++ project1.py
@@ -6,9 +6,35 @@ if answer == "yankees" or answer == "Yankees":
     print("Good job!")
 if answer == "red sox" or answer == "Red Sox" or answer == "Red sox":
     print("bad job, your wrong")
-print("who is your favorite Yankee?")
 input()
+print("who is your favorite Yankee?")
+answer = input()
 if answer == "Jazz Chisholm Jr." or answer == "jazz chisholm jr." or answer == "Jazz" or answer == "jazz":
     print("Good job!")
 else:
-    print("your wrong!")
+    print(" bad job, you should have known")
+input()
+print("is the Red Sox's Jarren Duran good? yes or no.")
+answer = input()
+if answer == "no" or answer == "No" or answer == "NO":
+    print("good")
+else:
+    print("you are wrong,")
+    input()
+    print("BAD JOB!!!")
+input()
+print("are the red sox overrated? yes or no")
+answer = input()
+if answer == "yes" or answer == "Yes" or answer == "YES":
+    print("good")
+    input()
+else:
+    print("no, bad job")
+    input()
+print("are the Red Sox bad? yes or no")
+input()
+if answer == "yes" or answer == "Yes" or answer == "YES":
+    print("good")
+else:
+    print("bad")
+print(f"thanks {name}, thats all!")
